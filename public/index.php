172bc80611c1e4c8dd9fe14484a4a6d7acc3f4bb
MattCouv <matth.couvreur@gmail.com>
2016-02-23 20:14:31 +0100

    index.html to test the livereload functionnalities

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@
     <title>Loadoutgenerator</title>
   </head>
   <body>
-
+    <h1>hello this is the home page</h1>
   </body>
+  <script src="./../bower_components/jquery/dist/jquery.min.js" charset="utf-8"></script>
+  <script src="./assets/js/swbfApp.js" charset="utf-8"></script>
+  <script src="./assets/js/bfApp.js" charset="utf-8"></script>
 </html>
