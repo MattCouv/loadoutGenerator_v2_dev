1b1b0872008ce0bbf7a077e1b11273c1d9cbf2d5
MattCouv <matth.couvreur@gmail.com>
2016-02-23 22:40:22 +0100

    controller that displays the home page

--- app/App.php
+++ app/App.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* 
+*
 */
 class App{
-	
+
 	public $title = " Mon site ";
 	private $db_instance;
 	private $router_instance;
@@ -14,14 +14,6 @@ class App{
 			self::$_instance = new App();
 		}
 		return self::$_instance;
-	}
-
-	public function getDb(){
-		$config = \Core\Config::getInstance(ROOT . '/Config/config.php');
-		if (is_null($this->db_instance)) {
-			$this->db_instance = new \Core\Database\Database($config->get('db_name'),$config->get('db_user'),$config->get('db_pass'),$config->get('db_host'));
-		}
-		return $this->db_instance;
 	}
 
 	public function getRouting(){
@@ -39,15 +31,5 @@ class App{
 		App\Autoloader::register();
 		require ROOT . '/Core/Autoloader.php';
 		Core\Autoloader::register();
-	}
-
-	public function forbidden(){
-		header('HTTP/1.0 403 Forbidden');
-		die('Acces interdit');
-	}
-
-	public function notFound(){
-		header('HTTP/1.0 404 Not Found');
-		die('Page introuvable');
 	}
 }
---
+++ app/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\controllers;
+/**
+ *
+ */
+class Home extends AppController
+{
+
+  public function index(){
+    $this->render( "home" );
+  }
+
+}
+
+ ?>
