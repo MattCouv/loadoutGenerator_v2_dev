b5b99393cbeda73db5ab9b4b12d6692cb3638657
MattCouv <matth.couvreur@gmail.com>
2016-02-23 22:49:33 +0100

    added temporary home page and default template

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,25 +1,196 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>project pi Login page</title>
-	<link rel="stylesheet" href="/structureMVC/public/css/home.css">
-</head>
-<body>
-	<header class="header">
-		<h1>Bienvenue sur</h1>
-		<h1 class="project">Project PI</h1>
-	</header>
-	<section class="card login">
-		<p id="msg"><?php if($errors){ echo 'Identifiant incorrect';}else{echo '';} ?></p>
-		<form class="form" method="post" autocomplete="off" id='login_form'>
-			<?= $form->input('login','Utilisateur ou email');?>
-			<?= $form->input('password','Mot de passe',['type'=>'password']);?>
-			<div>
-				<button type="submit" class="connect" id="submit">Connexion</button>
+<div class="container" id="login">
+<div id="help"><p>Get started it's easy ! </br>Enter your BF4 soldier name and select the platform you play on and submit ! </p></div>
+<div class="main">
+	Soldier Name :<span> (Case sensitive)</span><input type="text" id="soldiername" class="box-content active-borders">
+	Your Platform :
+	<select name="browser" class="box-content active-borders" id="platform">
+				<option value="pc">pc</option>
+			<option value="xbox">xbox</option>
+			<option value="ps3">ps3</option>
+			<option value="xone">xone</option>
+			<option value="ps4">ps4</option>
+	 </select>
+	<button id="sub" class="gen-border box-content">Submit</button>
+</div>
+</div>
+<div id="error" class="container">
+<p class="demo">Error you must input your soldier name and select your soldier's platform</p>
+</div>
+<div id="loadout" class="loadout-container-wpr">
+
+<div class="row">
+	<div class="loadout-kit">
+		<div class="loadout-container-header">
+			<h1><span><i></i>kit</span><span class="kitname"></span></h1>
+		</div>
+		<div class="kit-picture box-content gen"><div class="picture"></div></div>
+		<div class="gen box-content"><div class="gen-border"><h1><span>generate</span></h1></div></div>
+	</div>
+	<div class="loadout-item-container left-margin-container primary-gun">
+		<div class="loadout-container-header">
+			<h1><span>primary</span><span class="Pname"></span></h1>
+		</div>
+		<div class="gun-container box-content"><div class="gun primaryW"></div></div>
+		<div class="attachments">
+			<div class="optic box-content"></div>
+			<div class="accesory box-content box-content box-content box-content"></div>
+			<div class="barrel box-content box-content box-content"></div>
+			<div class="underbarrel box-content box-content"></div>
+			<div class="ammo box-content"></div>
+		</div>
+	</div>
+	<div class="loadout-item-container left-margin-container handgun">
+		<div class="loadout-container-header">
+			<h1><span>handgun</span><span class="Hname"></span></h1>
+		</div>
+		<div class="gun-container box-content"><div class="gun handgunW"></div></div>
+		<div class="attachments">
+			<div class="optic box-content"></div>
+			<div class="accesory box-content box-content box-content box-content"></div>
+			<div class="barrel box-content box-content box-content"></div>
+			<div class="underbarrel box-content box-content"></div>
+			<div class="ammo box-content"></div>
+		</div>
+	</div>
+	<div class="loadout-item-container left-margin-container items">
+		<div class="loadout-container-header">
+			<h1><span>items</span></h1>
+		</div>
+		<div class="item box-content">
+			<div class="picture-container">
+				<div class="itempic"></div>
 			</div>
-		</form>
-	</section>
-	<!-- <script src="/structureMVC/public/js/valScript.js"></script> -->
-</body>
-</html>
+			<div class="itemname">
+				<h1 class="name"></h1>
+			</div>
+		</div>
+		<div class="item box-content">
+			<div class="picture-container">
+				<div class="itempic"></div>
+			</div>
+			<div class="itemname">
+				<h1 class="name"></h1>
+			</div>
+		</div>
+		<div class="item box-content">
+			<div class="picture-container">
+				<div class="itempic"></div>
+			</div>
+			<div class="itemname">
+				<h1 class="name"></h1>
+			</div>
+		</div>
+		<div class="item box-content">
+			<div class="picture-container">
+				<div class="itempic"></div>
+			</div>
+			<div class="itemname">
+				<h1 class="name"></h1>
+			</div>
+		</div>
+		<div class="item box-content">
+			<div class="picture-container">
+				<div class="itempic"></div>
+			</div>
+			<div class="itemname">
+				<h1 class="name"></h1>
+			</div>
+		</div>
+	</div>
+</div>
+<table class="loadout-tablet">
+	<tbody>
+		<tr>
+			<td>
+				<div class="loadout-kit loadout-tablet-item-container">
+					<div class="loadout-container-header">
+						<h1><span><i></i>kit</span><span class="kitname"></span></h1>
+					</div>
+					<div class="kit-picture box-content gen"><div class="picture"></div></div>
+		<div class="gen box-content"><div class="gen-border"><h1><span>generate</span></h1></div></div>
+				</div>
+			</td>
+			<td>
+				<div class="loadout-tablet-item-container primary-gun">
+					<div class="loadout-container-header">
+						<h1><span>primary</span><span class="Pname"></span></h1>
+					</div>
+					<div class="gun-container box-content"><div class="gun primaryW"></div></div>
+					<div class="attachments">
+						<div class="optic box-content"></div>
+						<div class="accesory box-content box-content box-content box-content"></div>
+						<div class="barrel box-content box-content box-content"></div>
+						<div class="underbarrel box-content box-content"></div>
+						<div class="ammo box-content"></div>
+					</div>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="loadout-tablet-item-container handgun">
+					<div class="loadout-container-header">
+						<h1><span>handgun</span><span class="Hname"></span></h1>
+					</div>
+					<div class="gun-container box-content"><div class="gun handgunW"></div></div>
+					<div class="attachments">
+						<div class="optic box-content"></div>
+						<div class="accesory box-content box-content box-content box-content"></div>
+						<div class="barrel box-content box-content box-content"></div>
+						<div class="underbarrel box-content box-content"></div>
+						<div class="ammo box-content"></div>
+					</div>
+				</div>
+			</td>
+			<td>
+				<div class="loadout-tablet-item-container items">
+					<div class="loadout-container-header">
+						<h1><span>items</span></h1>
+					</div>
+					<div class="item box-content">
+						<div class="picture-container">
+							<div class="itempic"></div>
+						</div>
+						<div class="itemname">
+							<h1 class="name"></h1>
+						</div>
+					</div>
+					<div class="item box-content">
+						<div class="picture-container">
+							<div class="itempic"></div>
+						</div>
+						<div class="itemname">
+							<h1 class="name"></h1>
+						</div>
+					</div>
+					<div class="item box-content">
+						<div class="picture-container">
+							<div class="itempic"></div>
+						</div>
+						<div class="itemname">
+							<h1 class="name"></h1>
+						</div>
+					</div>
+					<div class="item box-content">
+						<div class="picture-container">
+							<div class="itempic"></div>
+						</div>
+						<div class="itemname">
+							<h1 class="name"></h1>
+						</div>
+					</div>
+					<div class="item box-content">
+						<div class="picture-container">
+							<div class="itempic"></div>
+						</div>
+						<div class="itemname">
+							<h1 class="name"></h1>
+						</div>
+					</div>
+				</div>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+</div>
--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="copyright" content="loadoutgenerator is created by Matthieu Couvreur and all rights are recerved to him">
+	<meta name="keywords" content="bf4,loadout,generator,loadoutgenerator,random,classes,guns,weapons,items,kits,gadgets,randomly">
+	<meta name="description" content="With loadoutgenerator you will be able to generate some random loadouts with YOUR unlocks for Battlefield 4 to mix things up and have some fun trying new ways to play.">
+	<meta name="robots" content="index,follow">
+	<title>LoadoutGenerator</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<!-- @@@ CSS -->
+	<link rel="stylesheet" type="text/css" href="./css/main.css" >
+</head>
+<body>
+<header id="head">
+	<div class="game"><a href="#"><img src="./img/logosmall.png" alt=""></div></a>
+	<div class="nav">
+	<div class="menu">
+	<div class="home">
+		<a href="#" class="active-borders-right" id="soldierBtn">Soldier</a>
+	</div>
+	<table class="left">
+		<tbody>
+			<tr>
+				<td>
+				<a href="about" class="active-borders-left" id="aboutBtn">About</a>
+				</td>
+				<td>
+				<a href="donation" class="active-borders-left" id="donateBtn">Support</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+	</div>
+</header>
 
-    <title>Starter Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-
-      </div>
-    </nav>
-
-    <div class="container" style="padding-top: 100px">
-<?= $content; ?>
-
-    </div><!-- /.container -->
+  <?= $content ?>
 
 
-  </body>
+
+  <!-- @@@ JS -->
+<script src="./../bower_components/jquery/dist/jquery.min.js" ></script>
+<script src="./js/App.js"></script>
+<!-- Analytics -->
+</body>
 </html>
