18304b0f2108ce7fc38f7e2ffd374f82f9f0f216
MattCouv <matth.couvreur@gmail.com>
2016-02-23 22:56:12 +0100

    set index to start php MVC app

--- public/index.php
+++ public/index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Loadoutgenerator</title>
-  </head>
-  <body>
-    <h1>hello this is the home page</h1>
-  </body>
-  <script src="./../bower_components/jquery/dist/jquery.min.js" charset="utf-8"></script>
-  <script src="./assets/js/swbfApp.js" charset="utf-8"></script>
-  <script src="./assets/js/bfApp.js" charset="utf-8"></script>
-</html>
+<?php
+	define('ROOT', dirname(__DIR__));
+	require ROOT.'/app/App.php';
+	$app = App::getInstance();
+	$app::load();
+	$app->getRouting();
+?>
